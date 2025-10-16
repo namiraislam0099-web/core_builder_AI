89f17ab0d4d288c81e24623dc6a9e068abde30c5
namiraislam0099-web <namiraislam0099@gmail.com>
2025-10-16 16:01:44 +0600

    Implement service worker for caching core files

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'core-builder-v1';
+
+const urlsToCache = [
+  'index.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Opened cache and cached core files');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
